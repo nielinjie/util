ae286a18be1184fc07341f44a85253bfc0107fe8
nielinjie <nielinjie@gmail.com>
2011-04-05 22:32:42 +0800

    change project structure

---
+++ data/project/build/Project.scala
@@ -0,0 +1,14 @@
+import sbt._
+
+class DataProject(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
+  val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
+  val akkaRepository = "akka repository" at "http://www.scalablesolutions.se/akka/repository/"
+  val ibiblioRepository = "ibiblio repository" at "http://www.ibiblio.org/maven/"
+  val wsoRepository = "wso" at "http://dist.wso2.org/maven2/"
+  def specs2Framework = new TestFramework("org.specs2.runner.SpecsFramework")
+
+  val specs = "org.specs2" %% "specs2" % "1.1-SNAPSHOT" % "test"
+
+  override def testFrameworks = super.testFrameworks ++ Seq(specs2Framework)
+
+}
