eade08ab6b4c92029aaf6fe692acec745c7440ac
nielinjie001 <nielinjie001@pingan.com.cn>
2011-04-13 16:23:16 +0800

    single selected for listView, general event stream, bind for selected item

--- ui/src/main/scala/events.scala
+++ ui/src/main/scala/events.scala
@@ -2,40 +2,94 @@ package nielinjie
 package util.ui
 package event
 
-import reactive.EventSource
-import swing.event.SelectionChanged
-import swing.{Publisher, Component, ListView}
+import swing.{Table, Publisher, Component, ListView}
+import reactive._
+import swing.event.{Event, SelectionChanged}
 
 object SelectSupport {
-  implicit def listViewSelectable[A] = new Selectable[A, ListView] {
-    def wrap(listView: ListView[A]) = new ComponentWithSelectSupport[A, ListView[A]](listView) {
-      listView.selection.intervalMode = ListView.IntervalMode.Single
-      listView.reactions += {
-        case SelectionChanged(`listView`) =>
-          if (!listView.selection.adjusting) {
-            selectChangedEvent.fire(listView.selection.items(0))
-          }
-      }
-    }
+
+  implicit def componentWithSelectSupport2Component[A, T](componentWithSelectSupport: ComponentWithSingleSelectSupport[A, T]): T = componentWithSelectSupport.component
+
+  implicit def componentWithSelectSupport2BindSelectedSupported[A, T](componentWithSelectSupport: ComponentWithSingleSelectSupport[A, T]): BindSelectedSupport[A, T] = new BindSelectedSupport(componentWithSelectSupport)
+  implicit def component2GeneralEventStreamSupport(component:Component): GeneralEventStreamSupport = new GeneralEventStreamSupport(component)
+
+  implicit def listView2SelectSupport[A](listView: ListView[A]) = new SelectableWrap[A, ListView[A]](listView) {
+    def singleSelect = new ListViewWithSingleSelectSupport[A](listView)
   }
 
-
-  def singleSelect[A, C[A] <: Component](component: C[A])(implicit selectable: Selectable[A, C]): ComponentWithSelectSupport[A, C[A]] =
-    selectable.wrap(component)
-
-  implicit def componentWithSelectSupport2Component[A, T](componentWithSelectSupport: ComponentWithSelectSupport[A, T]): T = componentWithSelectSupport.component
-//  implicit def component2SelectSupport[A,C[A]<: Component](component:C[A])(implicit selectable: Selectable[A, C])=
-//    new componentWrap(component,selectable)
-////  class componentWrap[A, C[A] <: Component](val component:C[A],val selectable: Selectable[A, C]){
-////    import SelectSupport._
-////    def singleSelect(component: C[A])=selectable.wrap(component)
-////  }
 }
 
-class ComponentWithSelectSupport[A, T](val component: T) {
-  val selectChangedEvent: EventSource[A] = new EventSource[A]() {}
+class ListViewWithSingleSelectSupport[A](val listView: ListView[A]) extends ComponentWithSingleSelectSupport[A, ListView[A]](listView) {
+  listView.selection.intervalMode = ListView.IntervalMode.Single
+  listView.selection.reactions += {
+    case SelectionChanged(`listView`) =>
+      if (!listView.selection.adjusting) {
+        selectChanging.fire(SingleSelecting(listView.selection.items.headOption, updating))
+      }
+  }
+
+  def setSelectedValue(value: A) = {
+    updating = true
+    val selectedIndex = listView.selection.indices.headOption
+    selectedIndex.foreach {
+      index =>
+        listView.listData = listView.listData.updated(index, value)
+        listView.selectIndices(index)
+    }
+    updating = false
+  }
+
+  //  def onModelChange(seqDelta:SeqDelta){
+  //    seqDelta match {
+  //      case Update(index,_,newV) =>
+  //
+  //      case Remove(index,_)
+  //      case Include(index,newV)
+  //    }
+  //  }
 }
 
-trait Selectable[A, C[A] <: Component] {
-  def wrap(component: C[A]): ComponentWithSelectSupport[A, C[A]]
+abstract class ComponentWithSingleSelectSupport[A, T](val component: T) {
+  var updating = false
+  val selectChanging: EventSource[SingleSelecting[A]] = new EventSource[SingleSelecting[A]]() {}
+  val selectChangedEvent = selectChanging.filter({
+    e =>
+      !e.updating
+  }).map({
+    e =>
+      e.selected
+  })
+  val selected: Signal[Option[A]] = selectChangedEvent.hold(None)
+
+  def setSelectedValue(value: A)
+
+  //  //TODO, refactory to set delta?
+  //  def onModelChange(seqDelta:SeqDelta)
+}
+
+case class SingleSelecting[A](val selected: Option[A], val updating: Boolean)
+
+class BindSelectedSupport[A, T](val componentWithSelectSupport: ComponentWithSingleSelectSupport[A, T]) extends Observing {
+
+  def bindSelected(from: Option[A] => Unit, eventStream: EventStream[A]): ComponentWithSingleSelectSupport[A, T] = {
+    componentWithSelectSupport.selectChangedEvent.foreach(from)
+    eventStream.foreach(a => componentWithSelectSupport.setSelectedValue(a))
+    componentWithSelectSupport
+  }
+  def bindSelected(from: Option[A] => Unit): ComponentWithSingleSelectSupport[A, T] = {
+    componentWithSelectSupport.selectChangedEvent.foreach(from)
+    componentWithSelectSupport
+  }
+}
+
+abstract class SelectableWrap[A, T <: Component](val component: T) {
+  def singleSelect: ComponentWithSingleSelectSupport[A, T]
+}
+
+class GeneralEventStreamSupport(val component:Component) {
+  def eventStream[A](f:PartialFunction[Event,A])={
+    val eventStream=new EventSource[A](){}
+    component.reactions+=f.andThen(a => eventStream.fire(a))
+    eventStream
+  }
 }
--- ui/src/main/scala/example/tables.scala
+++ ui/src/main/scala/example/tables.scala
@@ -3,23 +3,26 @@ package util.ui
 package example
 
 import swing._
-import event.{WindowClosed, EditDone, SelectionChanged}
-import reactive.Observing
-import util.ui.event.{ SelectSupport}
+import event.{ButtonClicked, WindowClosed, EditDone, SelectionChanged}
+import util.ui.event.{SelectSupport}
+import reactive.{Var, Observing}
 
 object Tables extends SimpleSwingApplication with ListAndText {
+
   import SelectSupport._
+
   def top = new MainFrame {
     title = "First Swing App"
     contents = new MigPanel("fill,debug", "[fill,:300:]", "[fill,:400:][][]") {
       add(listView, "wrap")
-      //      add(textBox, "wrap")
-      //      add(button, "wrap")
+      add(textBox, "wrap")
+      add(button, "wrap")
     }
 
   }
 
   reactions += {
+
     case WindowClosed(top) => {
       println("what?")
       System.exit(0)
@@ -33,7 +36,37 @@ trait ListAndText extends Observing {
   self: Tables.type =>
 
   import SelectSupport._
-  val listView = singleSelect(new ListView[String](List("A", "B", "C")))
-  listView.selectChangedEvent.foreach(println)
 
+  val model = Var(List("A", "B", "C"))
+  val listView = new ListView[String](model.value).singleSelect
+  model.change.foreach(listView.listData = _)
+  listView.selectChanging.foreach {
+    x =>
+      println(x.toString)
+  }
+  listView.selected.change.foreach {
+    x =>
+      println(x.toString)
+  }
+
+  val textBox = new TextField()
+  val button = new Button("Change")
+  val buttonStream = button.eventStream({
+    case ButtonClicked(_) =>
+      "foo"
+  })
+  listView.bindSelected({
+    x =>
+      textBox.text = x.toString
+  }, buttonStream)
+
+  //  {
+  //
+  //    reactions += {
+  //      case ButtonClicked(_) =>
+  //      //        model()=List("foo","bar")
+  //        listView.setSelectedValue("foo")
+  //    }
+  //
+  //  }
 }
