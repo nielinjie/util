3436c5e2f0bbe8636d5796c6885728697c5bcfe6
nielinjie001 <nielinjie001@pingan.com.cn>
2011-05-26 09:51:55 +0800

    change scalaz and specs2 to latest.release

--- data/project/build/Project.scala
+++ data/project/build/Project.scala
@@ -7,10 +7,10 @@ class DataProject(info: ProjectInfo) extends DefaultProject(info) with IdeaProje
   //val wsoRepository = "wso" at "http://dist.wso2.org/maven2/"
   def specs2Framework = new TestFramework("org.specs2.runner.SpecsFramework")
 
-  val specs = "org.specs2" %% "specs2" % "1.3" % "test"
+  val specs = "org.specs2" %% "specs2" % "latest.release" % "test"
 
   override def testFrameworks = super.testFrameworks ++ Seq(specs2Framework)
 
-  val scalazCore = "org.scalaz" %% "scalaz-core" % "6.0.RC1"
+  val scalazCore = "org.scalaz" %% "scalaz-core" % "latest.release"
 
 }
