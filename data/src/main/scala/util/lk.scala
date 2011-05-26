f1b104796fab19292880cd6561163590c77b9866
nielinjie001 <nielinjie001@pingan.com.cn>
2011-05-26 17:27:28 +0800

    lookup with kleisli

--- data/src/main/scala/util/lk.scala
+++ data/src/main/scala/util/lk.scala
@@ -5,7 +5,11 @@ import scalaz._
 
 object R {
   import Scalaz._
-  type SLK[A] = ReaderT[PartialApply1Of2[Validation, String]#Apply,Map[String,A],A]
-  def slk[A](f:(Map[String,A])=>Validation[String,A]) = ReaderT[PartialApply1Of2[Validation, String]#Apply,Map[String,A],A](f)
-
+  //type SLK[A] = ReaderT[PartialApply1Of2[Validation, String]#Apply,Map[String,A],A]
+  def slk[A](f:(Map[String,A])=>Validation[String,A]) =
+    kleisli[PartialApply1Of2[Validation, String]#Apply,Map[String,A],A](f)
+  def oslk[A](f:(Map[String,A])=>Validation[String,Option[A]]) =
+    kleisli[PartialApply1Of2[Validation, String]#Apply,Map[String,A],Option[A]](f)
+  def lookup(key:String)=oslk({map:Map[String,_] => map.get(key).success})
+  //def required = slk()
 }
--- data/src/test/scala/rSpecs.scala
+++ data/src/test/scala/rSpecs.scala
@@ -9,11 +9,14 @@ object RSepcs extends Specification {
   import Scalaz._
   import R._
 
+  val map = Map("a" -> 1)
   "slk" in {
-    val map = Map("a" -> 1)
     val look = slk[Int]({
       map => (map.getOrElse("a", 2)).success
     })
     look(map) must be equalTo((1).success)
+  }
+  "lookup" in {
+    lookup("a")(map) must be equalTo((1.some).success)
   }
 }
