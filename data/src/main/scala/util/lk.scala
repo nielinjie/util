db589c7c9900dbfba245f03732a42ad9630efbc0
nielinjie001 <nielinjie001@pingan.com.cn>
2011-05-27 17:42:12 +0800

    kleisli composite without luck

--- data/src/main/scala/util/lk.scala
+++ data/src/main/scala/util/lk.scala
@@ -6,10 +6,13 @@ import scalaz._
 object R {
   import Scalaz._
   //type SLK[A] = ReaderT[PartialApply1Of2[Validation, String]#Apply,Map[String,A],A]
-  def slk[A](f:(Map[String,A])=>Validation[String,A]) =
-    kleisli[PartialApply1Of2[Validation, String]#Apply,Map[String,A],A](f)
-  def oslk[A](f:(Map[String,A])=>Validation[String,Option[A]]) =
-    kleisli[PartialApply1Of2[Validation, String]#Apply,Map[String,A],Option[A]](f)
-  def lookup(key:String)=oslk({map:Map[String,_] => map.get(key).success})
+//  def kle[A,B](f:A => Validation[String,B])=
+//    kleisli[PartialApply1Of2[Validation, String]#Apply,A,B](f)
+//  def slk[A](f:(Map[String,A])=>Validation[String,A]) =
+//    kleisli[PartialApply1Of2[Validation, String]#Apply,Map[String,A],A](f)
+//  def oslk[A](f:(Map[String,A])=>Validation[String,Option[A]]) =
+//    kleisli[PartialApply1Of2[Validation, String]#Apply,Map[String,A],Option[A]](f)
+  def lookup(key:String)= kleisli[PartialApply1Of2[Validation, String]#Apply,Map[String,Int],Option[Int]]({map:Map[String,Int] => map.get(key).success})
+  def required() =  kleisli[PartialApply1Of2[Validation, String]#Apply,Option[Int],Int]({a:Option[Int] => a.toSuccess("required")})
   //def required = slk()
 }
--- data/src/test/scala/rSpecs.scala
+++ data/src/test/scala/rSpecs.scala
@@ -10,13 +10,38 @@ object RSepcs extends Specification {
   import R._
 
   val map = Map("a" -> 1)
-  "slk" in {
-    val look = slk[Int]({
-      map => (map.getOrElse("a", 2)).success
-    })
-    look(map) must be equalTo((1).success)
-  }
   "lookup" in {
-    lookup("a")(map) must be equalTo((1.some).success)
+    lookup("a")(map) must be equalTo ((1.some).success)
   }
+  "required" in {
+    ////    val k=((lookup("a"):Kleisli[PartialApply1Of2[Validation, String]#Apply,Map[String,Int],Option[Int]])
+    ////      .>=>
+    ////      (required():Kleisli[PartialApply1Of2[Validation, String]#Apply,Option[Int],Int]))
+    //      //.apply(map) must equalTo(1.success)
+    //
+    //     val f = ☆((n: Int) => (if (n % 2 == 0) None else Some((n + 1).toString)).toSuccess("what?")):Kleisli[PartialApply1Of2[Validation, String]#Apply,Int,String]
+    //    val g = ☆((s: String) => (if (List(5, 7) ∃ (_ == s.length)) None else Some("[" + s + "]")).toSuccess("kaka")):Kleisli[PartialApply1Of2[Validation, String]#Apply,String,String]
+    //
+    //    // Kleisli composition
+    //    (List(7, 78, 98, 99, 100, 102, 998, 999, 10000) map (f >=> g apply _)) must notNull
+    //      //equalTo (List(Some("[8]"), None, None, Some("[100]"), None, None, None, Some("[1000]"), None))
+
+    val f = kleisli((n: Map[String, Int]) => n.get("a"))
+    val g = kleisli((o: Int) => (o + 1).some)
+
+    (f >=> g).apply(Map("a" -> 1)) must equalTo(Some(2))
+
+    val f2 = kleisli((n: Map[String, Int]) => (n.get("a").toSuccess("not found")): Validation[String, Int])
+    val g2 = kleisli((o: Int) => ((o + 1).success): Validation[String, Int])
+
+    (f2 >=> g2).apply(Map("a" -> 1)) must equalTo((2).success)
+
+    
+    val f1 = kleisli[PartialApply1Of2[Validation, String]#Apply, Map[String, Int], Int]((n: Map[String, Int]) => (n.get("a").toSuccess("not found")): Validation[String, Int])
+    val g1 = kleisli[PartialApply1Of2[Validation, String]#Apply, Int, Int]((o: Int) => ((o + 1).success): Validation[String, Int])
+
+    (f1 >=> g1).apply(Map("a" -> 1)) must equalTo((2).success)
+
+  }
+
 }
